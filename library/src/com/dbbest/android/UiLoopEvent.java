d38d6f54790fcc481e041488efa201c85cc5a62b
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2013-11-25 17:41:27 +0200

    camera-video-recorder activity freezes after back pressing, fixed

--- library/src/com/dbbest/android/UiLoopEvent.java
+++ library/src/com/dbbest/android/UiLoopEvent.java
@@ -1,25 +1,32 @@
 package com.dbbest.android;
 
+import android.content.Context;
 import android.os.Handler;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by Tikhonenko.S on 31.10.13.
  */
 public class UiLoopEvent {
+    private static final long DEFAULT_DELAY = 30;
+
     private Runnable runnable;
     private Handler handler = new Handler();
     private Runnable onUi;
     private boolean isPaused = false;
+    private long delay;
+    private WeakReference<Context> context;
 
     private void runOnUiThread(){
         onUi = new Runnable() {
             @Override
             public void run() {
-                if(runnable != null){
+                if(runnable != null && (context == null || context.get() != null)){
                     if (!isPaused) {
                         runnable.run();
                     }
-                    handler.post(onUi);
+                    handler.postDelayed(onUi, delay);
                 }
             }
         };
@@ -27,16 +34,46 @@ public class UiLoopEvent {
         handler.post(onUi);
     }
 
+    // if context is null UiLoopEvent will work globally
+    public UiLoopEvent(Context context, long delay) {
+        setDelay(delay);
+        if (context != null) {
+            this.context = new WeakReference<Context>(context);
+        }
+    }
+
+    public UiLoopEvent(Context context) {
+        this(context, DEFAULT_DELAY);
+    }
+
+    public long getDelay() {
+        return delay;
+    }
+
+    public void setDelay(long delay) {
+        if(delay < 0){
+            throw new IllegalArgumentException();
+        }
+
+        this.delay = delay;
+    }
+
     public void run(Runnable runnable){
-        if(this.runnable != null){
-            handler.removeCallbacks(this.runnable);
+        if(onUi != null){
+            handler.removeCallbacks(onUi);
         }
 
         this.runnable = runnable;
-        runOnUiThread();
+        if (runnable != null) {
+            runOnUiThread();
+        }
     }
 
     public void stop(){
+       if(onUi != null){
+           handler.removeCallbacks(onUi);
+       }
+
         onUi = null;
         runnable = null;
     }
