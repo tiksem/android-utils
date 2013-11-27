afb1c8f43c63c7ed47b68d7314ff5857b15499ea
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2013-11-27 18:00:45 +0200

    DoubleActionHandler.java added, UiMessages message method added

--- library/src/com/dbbest/android/view/UiMessages.java
+++ library/src/com/dbbest/android/view/UiMessages.java
@@ -15,4 +15,13 @@ public class UiMessages {
         String message = context.getResources().getString(stringResourceId);
         error(context, message);
     }
+
+    public static void message(Context context, CharSequence text){
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void message(Context context, int stringResourceId){
+        String message = context.getResources().getString(stringResourceId);
+        error(context, message);
+    }
 }
