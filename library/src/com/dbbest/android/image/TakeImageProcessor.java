395c5ef3dcb3b305f509fbd348849bce32901754
malets.d@dbbest.com <bf9lfedyiz>
2013-11-29 16:38:49 +0200

    added new cropper

--- library/src/com/dbbest/android/image/TakeImageProcessor.java
+++ library/src/com/dbbest/android/image/TakeImageProcessor.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+
 import com.dbbest.android.analytics.L;
 import com.dbbest.android.image.listener.ImagePicked;
 import com.dbbest.android.image.listener.ImageProcessResult;
@@ -54,7 +55,11 @@ public class TakeImageProcessor {
     }
 
     public Intent createCropImageIntent(Uri inputImageUri, Uri outputImageUri) {
-        final Intent intent = new Intent("com.android.camera.action.CROP");
+        return createCropImageIntent("com.android.camera.action.CROP", inputImageUri, outputImageUri);
+    }
+
+    public Intent createCropImageIntent(String customAction, Uri inputImageUri, Uri outputImageUri) {
+        final Intent intent = new Intent(customAction);
         intent.setDataAndType(inputImageUri, "image/*");
 //        intent.putExtra("outputX", 400);
 //        intent.putExtra("outputY", 400);
@@ -64,9 +69,22 @@ public class TakeImageProcessor {
         intent.putExtra("noFaceDetection", true);
         intent.putExtra("return-data", true);
 
-        if (inputImageUri.equals(outputImageUri)) {
-            specifiedOutputUriForIntent(intent, outputImageUri);
-        }
+        specifiedOutputUriForIntent(intent, outputImageUri);
+        return intent;
+    }
+
+    public Intent createCropImageIntent(Context ctx, Class<?> component, Uri inputImageUri, Uri outputImageUri) {
+        final Intent intent = new Intent(ctx, component);
+        intent.setDataAndType(inputImageUri, "image/*");
+//        intent.putExtra("outputX", 400);
+//        intent.putExtra("outputY", 400);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.putExtra("noFaceDetection", true);
+        intent.putExtra("return-data", true);
+
+        specifiedOutputUriForIntent(intent, outputImageUri);
         return intent;
     }
 
@@ -148,11 +166,12 @@ public class TakeImageProcessor {
                     throwDeliveryException("Activity abort");
                 }
 
-                Bitmap resultImageBmp = intent.getExtras().getParcelable("data");
-                if (!isOutputUriSpecified) {
-                    throw new NullPointerException("Image destination uri - null, see setCurrentCropDestinationUri method");
-                }
-                savePickedImageTo(resultImageBmp, currentImageDestinationUri);
+//                Bitmap resultImageBmp = intent.getExtras().getParcelable("data");
+                Uri croppedImageUri = intent.getData();
+//                if (!isOutputUriSpecified) {
+//                    throw new NullPointerException("Image destination uri - null, see setCurrentCropDestinationUri method");
+//                }
+                savePickedImageTo(croppedImageUri, currentImageDestinationUri);
 
                 break;
         }
@@ -161,7 +180,7 @@ public class TakeImageProcessor {
     public void savePickedImageTo(Uri sourceUri, Uri destinationUri) {
         Context contextToProcess = mContextRef.get();
         if (contextToProcess != null) {
-            ImageProcessAsyncTask processImage = new ImageUriProcessAsyncTask(destinationUri);
+            ImageUriProcessAsyncTask processImage = new ImageUriProcessAsyncTask(destinationUri);
             if (imageProcessResultListener != null) {
                 processImage.setImageProcessResultListener(imageProcessResultListener);
             }
