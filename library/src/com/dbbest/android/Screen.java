c40f09cf313137134372b1a153a20a104c960f18
Dmytriy Manzhosov <manzhosov.d@dbbest.com>
2013-12-11 20:10:00 +0200

    Make calculate dimension via Windows Service.

--- library/src/com/dbbest/android/Screen.java
+++ library/src/com/dbbest/android/Screen.java
@@ -1,7 +1,13 @@
 package com.dbbest.android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.dbbest.framework.MathUtils;
 
 /**
@@ -10,27 +16,48 @@ import com.dbbest.framework.MathUtils;
  * Time: 13:46
  */
 public final class Screen {
-    public static DisplayMetrics getSize(Activity activity){
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        return displaymetrics;
-    }
 
-    public static DisplayMetrics getAspectRatio(Activity activity){
-        DisplayMetrics size = getSize(activity);
+    public static DisplayMetrics getAspectRatio(Context context){
+        DisplayMetrics size = getDisplayMetrics(context);
         int gcd = MathUtils.getGreatestCommonDivisor(size.widthPixels, size.heightPixels);
         size.widthPixels /= gcd;
         size.heightPixels /= gcd;
         return size;
     }
 
-    public static int getWidthFromHeight(Activity activity, int height){
-        DisplayMetrics displayMetrics = getSize(activity);
+    public static int getWidthFromHeight(Context activity, int height){
+        DisplayMetrics displayMetrics = getDisplayMetrics(activity);
         int width =  height * displayMetrics.widthPixels / displayMetrics.heightPixels;
         if(width % 2 == 1){
             width--;
         }
 
         return width;
+    }
+
+    public static int getScreenWidth(Context context) {
+        return getDisplayMetrics(context).widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        return getDisplayMetrics(context).heightPixels;
+    }
+
+    public static int getStatusBarHeight(Activity activity) {
+        Rect rectgle= new Rect();
+        Window window= activity.getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectgle);
+        int statusBarHeight = rectgle.top;
+//        int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+//        int titleBarHeight = contentViewTop - statusBarHeight;
+        return statusBarHeight;
+    }
+
+    public static DisplayMetrics getDisplayMetrics(Context context){
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        display.getMetrics(displaymetrics);
+        return displaymetrics;
     }
 }
