dd14fde1007c5befeefb3eeddbff9c10af352ac7
Tikhonenko Semyon <Tikhonenko.S@dbbest.com>
2014-04-16 18:11:15 +0300

    OrientationLandscapePortraitChangedListener.java added

---
+++ library/src/com/dbbest/android/OrientationLandscapePortraitChangedListener.java
@@ -0,0 +1,56 @@
+package com.dbbest.android;
+
+import android.content.Context;
+import android.content.res.Configuration;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 16.04.14
+ * Time: 17:48
+ */
+public abstract class OrientationLandscapePortraitChangedListener {
+    private final UiLoopEvent uiLoopEvent;
+    private int lastOrientation;
+    private Context context;
+
+    protected OrientationLandscapePortraitChangedListener(final Context context) {
+        uiLoopEvent = new UiLoopEvent(context);
+        this.context = context;
+    }
+
+    public void start(){
+        lastOrientation = context.getResources().getConfiguration().orientation;
+
+        uiLoopEvent.run(new Runnable() {
+            @Override
+            public void run() {
+                int currentOrientation = context.getResources().getConfiguration().orientation;
+
+                if (currentOrientation != lastOrientation) {
+                    if(currentOrientation == Configuration.ORIENTATION_LANDSCAPE){
+                        onLandscape();
+                    } else {
+                        onPortrait();
+                    }
+
+                    lastOrientation = currentOrientation;
+                }
+            }
+        });
+    }
+
+    public void stop(){
+        uiLoopEvent.stop();
+    }
+
+    public void pause(){
+        uiLoopEvent.pause();
+    }
+
+    public void resume(){
+        uiLoopEvent.resume();
+    }
+
+    public abstract void onLandscape();
+    public abstract void onPortrait();
+}
