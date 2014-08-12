b875dfdce696a341173a4d8fefb6a4423c288637
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2014-08-12 17:19:52 +0300

    MediaPicker.java improved

--- library/src/com/utilsframework/android/MediaPicker.java
+++ library/src/com/utilsframework/android/MediaPicker.java
@@ -2,7 +2,10 @@ package com.utilsframework.android;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.widget.Toast;
 
@@ -22,5 +25,24 @@ public final class MediaPicker {
         } catch (ActivityNotFoundException e) {
             Toast.makeText(activity, "Unsupported", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public static String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public static String getVideoPathFromPickVideo(Context context, Intent result) {
+        return getRealPathFromURI(context, result.getData());
     }
 }
