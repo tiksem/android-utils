524d70c4d2d76186dd841a522252a2bbc7776c7d
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2014-08-21 21:18:22 +0300

    MediaPlayerProgressUpdater.java added

---
+++ library/src/com/utilsframework/android/media/MediaPlayerProgressUpdater.java
@@ -0,0 +1,61 @@
+package com.utilsframework.android.media;
+
+import android.media.MediaPlayer;
+import com.utilsframework.android.UiLoopEvent;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 21.08.14
+ * Time: 21:06
+ */
+public abstract class MediaPlayerProgressUpdater {
+    private UiLoopEvent uiLoopEvent;
+    private long lastDuration = -1;
+    private long lastCurrentPosition = -1;
+
+    public void setMediaPlayer(final MediaPlayer mediaPlayer) {
+        if(mediaPlayer == null){
+            throw new NullPointerException();
+        }
+
+        if(uiLoopEvent != null){
+            uiLoopEvent.stop();
+        }
+
+        uiLoopEvent = new UiLoopEvent(mediaPlayer);
+        uiLoopEvent.run(new Runnable() {
+            @Override
+            public void run() {
+                boolean shouldCallUpdateListener = false;
+
+                try {
+                    long currentPosition = mediaPlayer.getCurrentPosition();
+                    if(lastCurrentPosition != currentPosition){
+                        lastCurrentPosition = currentPosition;
+                        shouldCallUpdateListener = true;
+                    }
+
+                } catch (IllegalStateException e) {
+
+                }
+
+                try {
+                    long currentDuration = mediaPlayer.getDuration();
+                    if(lastDuration != currentDuration){
+                        lastDuration = currentDuration;
+                        shouldCallUpdateListener = true;
+                    }
+
+                } catch (IllegalStateException e) {
+
+                }
+
+                if(shouldCallUpdateListener){
+                    onProgressChanged(lastCurrentPosition, lastDuration);
+                }
+            }
+        });
+    }
+
+    protected abstract void onProgressChanged(long progress, long max);
+}
