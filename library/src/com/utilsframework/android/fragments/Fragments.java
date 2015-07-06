2789b065210fc44182fefef00c379979dd6a8aa1
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-07-07 00:01:18 +0300

    createFragmentWith1Arg long

--- library/src/com/utilsframework/android/fragments/Fragments.java
+++ library/src/com/utilsframework/android/fragments/Fragments.java
@@ -238,6 +238,14 @@ public class Fragments {
         return fragment;
     }
 
+    public static <T extends android.support.v4.app.Fragment> T createFragmentWith1Arg(T fragment, String key,
+                                                                                       long value) {
+        Bundle args = new Bundle();
+        args.putLong(key, value);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     public static android.support.v4.app.Fragment getLatestFragmentInBackStack(FragmentActivity activity) {
         android.support.v4.app.FragmentManager fragmentManager = activity.getSupportFragmentManager();
         android.support.v4.app.FragmentManager.BackStackEntry backEntry =
