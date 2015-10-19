c93acf05f87888d0067dc44fb694f4c2077f3866
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-10-19 16:31:45 +0300

    EmptyRequestManager.java added

---
+++ library/src/com/utilsframework/android/network/EmptyRequestManager.java
@@ -0,0 +1,28 @@
+package com.utilsframework.android.network;
+
+import android.os.AsyncTask;
+import com.utilsframework.android.threading.OnFinish;
+import com.utilsframework.android.threading.Threading;
+import com.utilsframework.android.threading.ThrowingRunnable;
+
+import java.io.IOException;
+
+/**
+ * Created by stykhonenko on 19.10.15.
+ */
+public class EmptyRequestManager implements RequestManager {
+    @Override
+    public <Result> AsyncTask execute(Threading.Task<IOException, Result> task) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void execute(ThrowingRunnable<IOException> runnable, OnFinish<IOException> onFinish) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void cancelAll() {
+
+    }
+}
